8a9bd4885aed1d0917e71376df7199a765afc343
SAYEDABBAS12 <78164211+SAYEDABBAS12@users.noreply.github.com>
2022-04-01 16:57:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+
+function Start() {
+    document.getElementById("textbox").innerHTML = "";
+    recognition.start();
+}
+recognition.onresult = function (event) {
+    console.log(event);
+    var Content = event.results[0][0].transcript;
+    document.getElementById("textbox").innerHTML = Content;
+    console.log(Content);
+    speak()
+}
+
+function speak() {
+    var synth = window.speechSynthesis;
+    speak_data = document.getElementById("textbox").value;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+}
